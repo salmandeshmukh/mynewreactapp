b587bce0e3a0450762938af6cf143fcb9309c8a2
Salman Deshmukh <msalmand1@gmail.com>
2022-09-05 18:25:11 +0530

    add components

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Portfolio from './Portfolio'
+
+function About() {
+    return (
+        <>
+            <div className='container'>
+                <h1>About Us Component</h1>
+                <Portfolio />
+            </div>
+        </>
+    )
+}
+
+export default About
---
+++ src/components/Portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Portfolio = () => {
+    return (
+        <>
+
+            <h1>Portfolio Component</h1>
+
+        </>
+    )
+}
+
+export default Portfolio
